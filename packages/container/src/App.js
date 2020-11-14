23fc84f269a5468fbf375f98f131e79d304a76d2
Yağızhan Avcı <yagizhan.avci@crosstech.com.tr>
2020-11-14 13:57:39 +0300

    Randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </Router>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </Router>
+    </StylesProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
-    <StylesProvider>
+    // To prevent css class name collision add this prop
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route path="/" exact component={Landing} />

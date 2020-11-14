1035bf2f0f2621042432ae6d9dc92a940eed8517
Yağızhan Avcı <yagizhan.avci@crosstech.com.tr>
2020-11-14 23:16:27 +0300

    Added Auth Pages To Container & Added Loader Component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import Progress from "./components/Progress";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+
 export default () => {
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <Router>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
-      </Router>
+      <BrowserRouter>
+        <Header />
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={AuthLazy} />
+            <Route path="/" component={MarketingLazy} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
     </StylesProvider>
   );
 };
--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-console.log("Pipeline test");
-
 ReactDOM.render(<App />, document.querySelector("#root"));
